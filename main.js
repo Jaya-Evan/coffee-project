c8d6932aeeacc2c52f4c843e4c28de7474aa201f
jaya <jayaseyyadri@gmail.com>
2020-11-05 17:03:21 -0600

    finished ordering the coffee list

--- main.js
+++ main.js
@@ -11,12 +11,14 @@ function renderCoffee(coffee) {
 }
 
 function renderCoffees(coffees) {
+    var reversed=coffees.reverse()
     var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
+    for(var i = coffees.length-1; i >= 0; i--) {
+        html += renderCoffee(reversed[i]);
     }
     return html;
 }
+
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
